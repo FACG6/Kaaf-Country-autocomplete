0de91b52c1fc54c737a5c3bb520ba5a56e04a935
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-01-29 16:21:38 +0200

    adding the handelServe function

--- src/routerr.js
+++ src/routerr.js
@@ -3,6 +3,8 @@ const router = (request, response) => {
     const endpoint = request.url;
     if (endpoint === '/') {
        handler.handlehomePage(request, response);
+    } else if (endpoint.includes('public')){
+        handler.handelServe(request, response);
     } else {
         handler.handleNotFond(request, response);
     }
