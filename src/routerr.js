1497f8d9ffc699aa9f15b96253bfde8ecd54fd51
Khader <kakastarcris@gmail.com>
2019-01-30 13:31:51 +0200

    modify files

--- src/routerr.js
+++ src/routerr.js
@@ -1,14 +1,15 @@
-const handler = require('./handler');
+const handler = require("./handler");
+
 const router = (request, response) => {
-    const endpoint = request.url;
-    if (endpoint === '/') {
-        handler.handlehomePage(request, response);
-    } else if (endpoint.includes('public')) {
-        handler.handelServe(request, response);
-    } else if (endpoint === "/Country"){
-        handler.handelJsonFile(request, response);
-    } else {
-        handler.handleNotFond(request, response);
-    }
-}
+  const endpoint = request.url;
+  if (endpoint === "/") {
+    handler.handlehomePage(request, response);
+  } else if (endpoint.includes("public")) {
+    handler.handelServe(request, response);
+  } else if (endpoint === "/Country") {
+    handler.handelJsonFile(request, response);
+  } else {
+    handler.handleNotFond(request, response);
+  }
+};
 module.exports = router;
