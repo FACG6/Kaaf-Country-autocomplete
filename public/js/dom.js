5a1d2a9bbab8c0d6ffdf24ca8826fe647f1faaf9
Khader <kakastarcris@gmail.com>
2019-01-31 10:30:24 +0200

    white spaces

--- public/js/dom.js
+++ public/js/dom.js
@@ -4,19 +4,18 @@ const name = document.querySelector(".name");
 const capital = document.querySelector(".capital");
 const currncy = document.querySelector(".currncy");
 const pop = document.querySelector(".pop");
+
 input.addEventListener("input", () => {
   dataResult.innerHTML = "";
-  if (!input.value.trim())
-    return;
-  dataRequist("Get", "/Country", (response) => {
-    dataResult.innerHTML = "";
+  if (!input.value.trim()) return;
+  dataRequist("Get", "/Country", response => {
     response.filter(item => {
       if (item.country.startsWith(input.value.trim().charAt(0).toUpperCase())) {
         const listItem = document.createElement("option");
         listItem.textContent = item.country;
         dataResult.appendChild(listItem);
-      };
-    })
+      }
+    });
     render(response);
-  })
-})
+  });
+});
